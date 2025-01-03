e5f35eb8fd4c8481b8a16d0d3b71e9c660f3c682
DahnDell <khanman1208@gmail.com>
2025-01-03 16:19:52 +0900

    index js / app js / react.txt 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Welcome from "./chapter_05/Welcome";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hm....
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Welcome name = "Mike"/>
+      <Welcome name = "Steve"/>
+      <Welcome name = "Jane"/>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter_04/Clock';
+import AirBnBList from './chapter_05_2/AirBnBList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 setInterval(()=> {
+  
   root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <MyElement /> */}
-    {/* {<Library />} */}
-    <Clock/>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+      {/* <AirBnBList/>
+       */}
+       <NotificationList />
+    </React.StrictMode>
+  );
+
 });
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
